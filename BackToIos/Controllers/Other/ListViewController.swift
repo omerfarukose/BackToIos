c39d4f4a01c8c1e8743db27eb7a14764ddb8b626
Ömer Faruk KÖSE <omerfarukose@Omers-MacBook-Air.local>
2023-06-11 12:56:26 +0300

    view controllers created

---
+++ BackToIos/Controllers/Other/ListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListViewController.swift
+//  BackToIos
+//
+//  Created by Ömer Faruk KÖSE on 11.06.2023.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
